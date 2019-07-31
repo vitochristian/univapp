b78ccfe3305680b967d0e3f0830feaf3465b8db3
Rails Udemy Course <rails@gmail.com>
2019-07-31 16:59:58 +0700

    add validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
 	# model name must be singular while table name must be plural
+	validates :short_name, presence: true, length: {minimum: 3, maximum: 15}
+	validates :name, presence: true, length: {minimum: 5, maximum: 50}
+	validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
 end
