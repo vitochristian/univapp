94cfa525ba7ebdd00b4cec870397de5467c341aa
Rails Udemy Course <rails@gmail.com>
2019-08-01 08:40:48 +0700

    Create students table and student model

---
+++ app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ApplicationRecord
+	before_save {self.email = email.downcase}
+	validates :name, presence: true, length: {minimum: 3, maximum: 50}
+	# regular exp to validate email --> must contain ...@.. .com
+	# caps words mean constant var in rails 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+	validates :email, presence: true, length: { maximum: 105 },            
+                     uniqueness: { case_sensitive: false },            
+                       format: { with: VALID_EMAIL_REGEX }
+end
