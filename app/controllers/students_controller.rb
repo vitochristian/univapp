e6970d9e37a6236d795df889cd3ba4cd7a93abab
Rails Udemy Course <rails@gmail.com>
2019-08-01 11:48:39 +0700

    Create students and show errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,25 @@ class StudentsController < ApplicationController
 		@student = Student.new
 	end 
 
+	def create
+		@student = Student.new(stud_params)
+		# condition if student obj is saved
+		if @student.save
+			# give a success message
+			flash[:success] = "You have successfully signed up"
+			redirect_to root_path
+		# condition if it fails to save
+		else
+			# open new sign up template
+			render 'new'
+		end
+
+	end
+
+	private
+	# whitelist the student obj so it can be saved
+	def stud_params
+		# the parameters require obj student to exist and allowing name and email to be save to the obj later on
+		params.require(:student).permit(:name, :email)
+	end
 end
