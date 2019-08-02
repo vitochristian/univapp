8789bc16671d2f78e3d41528450d8c40600a767f
Rails Udemy Course <rails@gmail.com>
2019-08-02 10:18:38 +0700

    Add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
-  resources :students
+  resources :students, except: [:destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
